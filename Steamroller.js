7bbc15bc8df49ab09f50ec3622153ed5e80708ba
shannonliang312 <shannonliang312@gmail.com>
2017-02-04 15:32:20 +0800

    Steamroller

---
+++ Steamroller.js
@@ -0,0 +1,24 @@
+function steamrollArray(arr) {
+  // I'm a steamroller, baby
+  var res = [];
+
+  (function flatArray(arr) {
+    var tmp = 0;
+
+    for (var i = 0; i < arr.length; i++) {
+      tmp = arr[i];
+      if(Array.isArray(tmp) == true) {
+        flatArray(tmp);
+      }
+      else {
+        res.push(tmp);
+      }
+    }
+  })(arr);
+
+  return res;
+}
+
+
+
+console.log(steamrollArray([1, [2], [3, [[4]]]]));
